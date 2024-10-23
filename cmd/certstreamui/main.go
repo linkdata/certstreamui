85b8b6b3ee516e31382dbc4c29b1cd7bfe71253f
Johan Lindh <johan@linkdata.se>
2024-10-23 12:46:21 +0200

    wip

--- cmd/certstreamui/main.go
+++ cmd/certstreamui/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -12,22 +13,36 @@ import (
 
 	_ "net/http/pprof"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/linkdata/certstream/certdb"
 	"github.com/linkdata/certstreamui"
 	"github.com/linkdata/deadlock"
 	"github.com/linkdata/jaws"
 	"github.com/linkdata/webserv"
 )
 
+func env(key, dflt string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		val = dflt
+	}
+	return os.ExpandEnv(val)
+}
+
 var (
-	flagAddress    = flag.String("address", os.Getenv("WEBSERV_LISTEN"), "serve HTTP requests on given [address][:port]")
-	flagCertDir    = flag.String("certdir", os.Getenv("WEBSERV_CERTDIR"), "where to find fullchain.pem and privkey.pem")
-	flagUser       = flag.String("user", os.Getenv("WEBSERV_USER"), "switch to this user after startup (*nix only)")
-	flagDataDir    = flag.String("datadir", os.Getenv("WEBSERV_DATADIR"), "where to store data files after startup")
-	flagListenURL  = flag.String("listenurl", os.Getenv("WEBSERV_LISTENURL"), "manually specify URL where clients can reach us")
+	flagAddress    = flag.String("address", env("WEBSERV_LISTEN", ""), "serve HTTP requests on given [address][:port]")
+	flagCertDir    = flag.String("certdir", env("WEBSERV_CERTDIR", ""), "where to find fullchain.pem and privkey.pem")
+	flagUser       = flag.String("user", env("WEBSERV_USER", ""), "switch to this user after startup (*nix only)")
+	flagDataDir    = flag.String("datadir", env("WEBSERV_DATADIR", ""), "where to store data files after startup")
+	flagListenURL  = flag.String("listenurl", env("WEBSERV_LISTENURL", ""), "manually specify URL where clients can reach us")
 	flagVersion    = flag.Bool("v", false, "display version")
 	flagCpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 	flagMemprofile = flag.String("memprofile", "", "write memory profile to file")
 	flagPprof      = flag.Bool("pprof", false, "run pprof on http://localhost:6060/debug/pprof/")
+	flagDbUser     = flag.String("dbuser", env("DBUSER", "certstream"), "database user")
+	flagDbPass     = flag.String("dbpass", env("DBPASS", "certstream"), "database password")
+	flagDbName     = flag.String("dbname", env("DBNAME", "certstream"), "database name")
+	flagDbAddr     = flag.String("dbaddr", env("DBADDR", ""), "database address")
 )
 
 func main() {
@@ -52,6 +67,23 @@ func main() {
 			slog.Error(http.ListenAndServe("localhost:6060", nil).Error())
 		}()
 		slog.Info("pprof listening on http://localhost:6060/debug/pprof/")
+	}
+
+	if *flagDbAddr != "" {
+		dsn := fmt.Sprintf("postgres://%s:%s@%s/%s", *flagDbUser, *flagDbPass, *flagDbAddr, *flagDbName)
+		db, err := sql.Open("pgx", dsn)
+		if err == nil {
+			if err = db.Ping(); err == nil {
+				var cdb *certdb.Certdb
+				if cdb, err = certdb.New(context.Background(), db); err == nil {
+					cdb.Close()
+				}
+			}
+			db.Close()
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	cfg := &webserv.Config{
--- go.mod
+++ go.mod
@@ -3,21 +3,29 @@ module github.com/linkdata/certstreamui
 go 1.23.2
 
 require (
-	github.com/linkdata/certstream v0.0.4
+	github.com/jackc/pgx/v5 v5.7.1
+	github.com/linkdata/certstream v0.0.5
 	github.com/linkdata/deadlock v0.5.2
 	github.com/linkdata/jaws v0.79.0
 	github.com/linkdata/webserv v0.9.7
 )
+
+replace github.com/linkdata/certstream => ../certstream
 
 require (
 	github.com/coder/websocket v1.8.12 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/google/certificate-transparency-go v1.2.1 // indirect
 	github.com/google/trillian v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/petermattis/goid v0.0.0-20240813172612-4fcff4a6cae7 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/grpc v1.64.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
